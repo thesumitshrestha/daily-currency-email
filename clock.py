e4f268d2255d797f9c693fe1605443ad6b7af344
Sumit Shrestha <summitshrestha8@gmail.com>
2022-09-27 10:41:37 -0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -4,9 +4,9 @@ from currency_scapper import send_email
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=8)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=10:45)
 def scheduled_job():
-    print('This job is run every weekday at 8 AM.')
+    print('This job is run every weekday at 10:45 AM.')
     send_email()
 
 
